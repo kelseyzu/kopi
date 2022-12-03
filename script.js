da1f1df56a3d77e8c2d3f507de68297c31a3145e
kelsey zuberbuehler <zuber2@pdx.edu>
2022-12-02 23:52:57 -0800

    did something weird

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//this is my code
+$(document).ready(function() {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
