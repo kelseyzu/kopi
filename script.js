be88eb6a493678f554d7674c3dcbf8b8a3b37fab
kelsey zuberbuehler <zuber2@pdx.edu>
2022-12-05 18:52:46 -0800

    everything added

--- script.js
+++ script.js
@@ -28,3 +28,20 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10); 
+    value = isNaN(value)? 0 : value;
+    value ++;
+    input.value = value;
+  }
+  function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10); 
+    if (value > 1) {
+      value = isNaN(value)? 0 : value;
+      value --;
+      input.value = value;
+    }
+  }
